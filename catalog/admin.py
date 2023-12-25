cc41f499977adffc8f1511985b7993ca0d06956b
January <nameisjanuary@gmail.com>
2023-12-25 11:44:36 +0400

    added cats, dogs

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cat, Dog, Feedback
+admin.site.register(Cat)
+admin.site.register(Dog)
+admin.site.register(Feedback)
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Cat, Dog
+def index(request):
+    cat_list = Cat.objects.all()
+    dog_list = Dog.objects.all()
+    context = {
+        "cat_list": cat_list, "dog_list": dog_list
+    }
+    return render(request, "catalog/index.html", context)
+
