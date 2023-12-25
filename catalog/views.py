d9094cdc154196a28a12aa2d8c7680c06300b2b3
January <nameisjanuary@gmail.com>
2023-12-25 14:20:20 +0400

    added feedback

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-
-from .models import Cat, Dog
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.utils import timezone
+from .models import Cat, Dog, Feedback
+def feedback(request):
+    return render(request, "catalog/feedback.html")
+    
 def index(request):
     cat_list = Cat.objects.all()
     dog_list = Dog.objects.all()
@@ -9,3 +13,14 @@ def index(request):
     }
     return render(request, "catalog/index.html", context)
 
+def feedback_success(request):\
+    return render(request, "catalog/feedback_success.html")
+
+def feedback_submit(request):
+    print(request.POST)
+    feedback_model = Feedback(username = request.POST['name'], 
+                              email = request.POST['email'], 
+                              message = request.POST['message'],
+                              date_sent = timezone.now())
+    feedback_model.save()
+    return redirect('/catalog/feedback/success')
